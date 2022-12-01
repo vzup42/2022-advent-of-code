23a0783cfaf25b702c42f8370142ed1d59a2d379
valeriia.zakhlieniuk <valeriia.zakhlieniuk@up42.com>
2022-12-01 13:14:24 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
+    fun summarizeCaloriesOfTopElves(input: List<String>, elvesToInvolve: Int): Int {
+        val separatorIndicies = input.indices.filter { input[it].isEmpty() }
+        val leftIntervalSides = listOf(0) + separatorIndicies.map { it + 1 }
+        val rightIntervalSides = separatorIndicies + input.size
+        require(leftIntervalSides.size == rightIntervalSides.size)
+
+        return leftIntervalSides.indices.map { index ->
+            input.subList(leftIntervalSides[index], rightIntervalSides[index]).map(String::toInt).sum()
+        }.sortedDescending().take(elvesToInvolve).sum()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return summarizeCaloriesOfTopElves(input, 1)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return summarizeCaloriesOfTopElves(input, 3)
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
