1c8d00d6f4b37f5e0f9429659e18048d10c2ee62
valeriia.zakhlieniuk <valeriia.zakhlieniuk@up42.com>
2022-12-03 11:31:43 +0100

    Day 2: remove unneeded class

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -1,17 +1,15 @@
 package day02
 
-import day02.Game.Companion.against
-import day02.Game.Companion.by
-import day02.Game.Outcome.DRAW
-import day02.Game.Outcome.LOSS
-import day02.Game.Outcome.VICTORY
-import day02.Game.Shape.PAPER
-import day02.Game.Shape.ROCK
-import day02.Game.Shape.SCISSORS
+import day02.Outcome.DRAW
+import day02.Outcome.LOSS
+import day02.Outcome.VICTORY
+import day02.Shape.PAPER
+import day02.Shape.ROCK
+import day02.Shape.SCISSORS
 import readInput
 
 fun main() {
-    fun parseShape(shape: String) = when(shape) {
+    fun parseShape(shape: String) = when (shape) {
         "A", "X" -> ROCK
         "B", "Y" -> PAPER
         "C", "Z" -> SCISSORS
@@ -55,32 +53,28 @@ fun main() {
     println(part2(input))
 }
 
-class Game {
-    enum class Shape(val score: Int) {
-        ROCK(1), PAPER(2), SCISSORS(3);
-    }
-
-    enum class Outcome(val score: Int) {
-        LOSS(0), DRAW(3), VICTORY(6);
-    }
-
-    companion object {
-        private val outcomes: Map<Shape, Map<Shape, Outcome>> = mapOf(
-            ROCK to mapOf(ROCK to DRAW, PAPER to LOSS, SCISSORS to VICTORY),
-            PAPER to mapOf(PAPER to DRAW, SCISSORS to LOSS, ROCK to VICTORY),
-            SCISSORS to mapOf(SCISSORS to DRAW, ROCK to LOSS, PAPER to VICTORY)
-        )
-
-        private val shapes: Map<Outcome, Map<Shape, Shape>> = mapOf(
-            VICTORY to mapOf(ROCK to PAPER, PAPER to SCISSORS, SCISSORS to ROCK),
-            DRAW to mapOf(ROCK to ROCK, PAPER to PAPER, SCISSORS to SCISSORS),
-            LOSS to mapOf(ROCK to SCISSORS, PAPER to ROCK, SCISSORS to PAPER),
-        )
-
-        // Determines the game result for the currently selected shape by an opponent's shape.
-        infix fun Shape.against(opponentShape: Shape) = outcomes[this]!![opponentShape]!!
-
-        // Defines which shape to select by an opponent's shape in order to achieve the needed outcome
-        infix fun Outcome.by(opponentShape: Shape) = shapes[this]!![opponentShape]!!
-    }
+enum class Shape(val score: Int) {
+    ROCK(1), PAPER(2), SCISSORS(3);
 }
+
+enum class Outcome(val score: Int) {
+    LOSS(0), DRAW(3), VICTORY(6);
+}
+
+private val outcomes: Map<Shape, Map<Shape, Outcome>> = mapOf(
+    ROCK to mapOf(ROCK to DRAW, PAPER to LOSS, SCISSORS to VICTORY),
+    PAPER to mapOf(PAPER to DRAW, SCISSORS to LOSS, ROCK to VICTORY),
+    SCISSORS to mapOf(SCISSORS to DRAW, ROCK to LOSS, PAPER to VICTORY)
+)
+
+private val shapes: Map<Outcome, Map<Shape, Shape>> = mapOf(
+    VICTORY to mapOf(ROCK to PAPER, PAPER to SCISSORS, SCISSORS to ROCK),
+    DRAW to mapOf(ROCK to ROCK, PAPER to PAPER, SCISSORS to SCISSORS),
+    LOSS to mapOf(ROCK to SCISSORS, PAPER to ROCK, SCISSORS to PAPER),
+)
+
+// Determines the game result for the currently selected shape by an opponent's shape.
+infix fun Shape.against(opponentShape: Shape) = outcomes[this]!![opponentShape]!!
+
+// Defines which shape to select by an opponent's shape in order to achieve the needed outcome
+infix fun Outcome.by(opponentShape: Shape) = shapes[this]!![opponentShape]!!
